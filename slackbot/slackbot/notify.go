538d13b9c7ec5d3439214e5f7feb3707b725ac63
Chad Obrien <chad.obrien@pwc.com>
2020-03-09 16:32:47 +1100

    DOPS-1197 Update Output

--- slackbot/slackbot/notify.go
+++ slackbot/slackbot/notify.go
@@ -18,17 +18,21 @@ func Notify(b *cloudbuild.Build, webhook string, project string) {
 	var branch_string string
 	branch_string = "Repo details not found"
 
+	fmt.Printf("Start")
+
 	fmt.Printf("%+v\n", b)
-	//if len(b.Source.RepoSource) > 0 {
-	//
-	//	branch_string = "Repo details" + b.Source.RepoSource
-	//	if len(b.Source.RepoSource.BranchName) > 0 {
-	//		branch_string = fmt.Sprintf(`{"text": Branch: %s"}`, b.Source.RepoSource.BranchName)
-	//	}
-	//	if len(b.Source.RepoSource.TagName) > 0 {
-	//		branch_string = fmt.Sprintf(`{"text": Tag: %s"}`, b.Source.RepoSource.TagName)
-	//	}
-	//}
+	fmt.Printf("%+v\n", b.Source)
+	fmt.Printf("end")
+	if len(b.Source.RepoSource) > 0 {
+
+		branch_string = "Repo details" + b.Source.RepoSource
+		if len(b.Source.RepoSource.BranchName) > 0 {
+			branch_string = fmt.Sprintf(`{"text": Branch: %s"}`, b.Source.RepoSource.BranchName)
+		}
+		if len(b.Source.RepoSource.TagName) > 0 {
+			branch_string = fmt.Sprintf(`{"text": Tag: %s"}`, b.Source.RepoSource.TagName)
+		}
+	}
 
 	switch b.Status {
 	case "SUCCESS":
