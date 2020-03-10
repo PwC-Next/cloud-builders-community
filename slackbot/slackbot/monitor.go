db0344f8714e83d8762cc132bfbb3cac89e029cb
Chad Obrien <chad.obrien@pwc.com>
2020-03-10 13:20:38 +1100

    DOPS-1197 Update Output

--- slackbot/slackbot/monitor.go
+++ slackbot/slackbot/monitor.go
@@ -36,18 +36,17 @@ func Monitor(ctx context.Context, build string, webhook string) {
 
 		//GET https://cloudbuild.googleapis.com/v1/projects/{projectId}/triggers/{triggerId}
 		//Returns t which is a BuildTrigger:  BuildTrigger struct: https://cloud.google.com/cloud-build/docs/api/reference/rest/v1/projects.triggers#BuildTrigger
-		var trig
-		if b.BuildTriggerId != "" {
-			lc2 := svc.Projects.Triggers.Get(project, b.BuildTriggerId)
-			trig, err := lc2.Do()
-			if err != nil {
-				if errors <= maxErrors {
-					log.Printf("Failed to get build details from Cloud Build.  Will retry in one minute.")
-					errors++
-					continue
-				} else {
-					log.Fatalf("Reached maximum number of errors (%d).  Exiting", maxErrors)
-				}
+		//var trig
+
+		lc2 := svc.Projects.Triggers.Get(project, b.BuildTriggerId)
+		trig, err := lc2.Do()
+		if err != nil {
+			if errors <= maxErrors {
+				log.Printf("Failed to get build details from Cloud Build.  Will retry in one minute.")
+				errors++
+				continue
+			} else {
+				log.Fatalf("Reached maximum number of errors (%d).  Exiting", maxErrors)
 			}
 		}
 
